2a488629e5f932c7ea0f7ecc404009a5d0fee0ba
tpebop54 <tpebop54@gmail.com>
2025-03-13 16:08:57 -0600

    Update gg_evt.js

--- gg_evt.js
+++ gg_evt.js
@@ -1,5 +1,6 @@
 // Taken and mofidied from https://miraclewhips.dev/geoguessr-event-framework/geoguessr-event-framework.js
 
+
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
