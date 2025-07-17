a65c36b860128c22fa83f46a02940f854808979a
tpebop <tpebop54@gmail.com>
2025-07-17 14:17:39 -0600

    1.0.15

--- installer_dev.js
+++ installer_dev.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Tpebop's GeoGuessr Mods (DEV)
+// @name         Tpebop's GeoGuessr Mods (DEV 1.0.15)
 // @description  Various mods to make the game interesting in various ways
 // @version      1.0.15
 // @author       tpebop
